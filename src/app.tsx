551d42cab29cc4083d67fcd4f9e9c13c05a289d5
Kayan <kayan25@seas.upenn.edu>
2024-02-29 23:49:00 -0500

    Update

--- src/app.tsx
+++ src/app.tsx
@@ -8,37 +8,16 @@ function App() {
   const [posts, setPosts] = useState<Post[]>([]);
 
   const addPost = (newPost: Post) => {
-    if (newPost.parentId === null) {
-      setPosts([...posts, { ...newPost, votes: 0, replies: [] }]);
-    } else {
-      setPosts(updatePosts(posts, newPost));
-    }
+    setPosts([...posts, { ...newPost, id: Date.now(), votes: 0 }]);
   };
 
-  function updatePosts(posts: Post[], newPost: Post): Post[] {
-    return posts.map(post => {
-      if (post.id === newPost.parentId) {
-        return { ...post, replies: [...(post.replies || []), newPost] };
-      } else if (post.replies) {
-        return { ...post, replies: updatePosts(post.replies, newPost) };
-      }
-      return post;
-    });
-  }
-
-  function updateVotes(posts: Post[], id: number, delta: number): Post[] {
-    return posts.map(post => {
+  const handleVote = (id: number, delta: number) => {
+    setPosts(posts.map(post => {
       if (post.id === id) {
         return { ...post, votes: post.votes + delta };
-      } else if (post.replies) {
-        return { ...post, replies: updateVotes(post.replies, id, delta) };
       }
       return post;
-    });
-  }
-
-  const handleVote = (id: number, delta: number) => {
-    setPosts(updateVotes(posts, id, delta));
+    }));
   };
 
   return (
